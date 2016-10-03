e414414642459c4d65fc741284d3369790f34d78
Lennart Bernhardt <l.bernhardt@live.de>
2016-10-04 01:12:06 +0200

    Added poster to notication title

--- fp_TickerNotifier.user.js
+++ fp_TickerNotifier.user.js
@@ -2,7 +2,7 @@
 // @name         FP Ticker Notifier
 // @namespace    facepunch.com
 // @include       *facepunch.com/fp_ticker*
-// @version      0.3
+// @version      0.4
 // @description  Notifies you of new posts in threads you read while you have the ticker open
 // @author       Lennart Bernhardt (github.com/LennyPenny)
 // @updateURL    https://github.com/LennyPenny/FP-Ticker-Notifier/raw/master/fp_TickerNotifier.user.js
@@ -18,16 +18,14 @@
     var OldAddTickerPost = AddTickerPost;
     setTimeout(function() { //hope you have a fast enough connection
         AddTickerPost = function(post) {
-            if (post.attributes.getNamedItem( "html" ).value.includes("Last Read")) {
-                console.log("new post!");
-
+            if (post.attributes.getNamedItem( "html" ).value.includes("Last Read")){
                 //this is probably a shitty way to do this
                 var el = document.createElement("html");
                 el.innerHTML = post.attributes.getNamedItem( "html" ).value;
                 var links = el.getElementsByTagName("a");
 
                 if (links !== undefined)
-                    GM_notification(links[links.length-2].innerHTML, "New post!", "https://facepunch.com/fp/forums/6.png", function() {
+                    GM_notification(links[links.length-2].innerHTML, "New post by " + links[links.length-3].innerHTML, "https://facepunch.com/fp/forums/6.png", function() {
                         GM_openInTab(links[links.length-1].href);
                     });
             }
